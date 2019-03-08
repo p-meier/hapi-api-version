43520943ea53416c018bd216d962822aa14a8f2d
Tim Costa <tim@timcosta.io>
2019-03-08 17:49:40 +0000

    Hapi 18 Compatibility (#28)

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ exports.register = (server, options) => {
         const route = server.match(method, versionedPath);
 
         if (route && route.path.indexOf(options.basePath + 'v' + requestedVersion + '/') === 0) {
-            request.setUrl(options.basePath + 'v' + requestedVersion + request.url.path.slice(options.basePath.length - 1)); //required to preserve query parameters
+            request.setUrl(options.basePath + 'v' + requestedVersion + request.path.slice(options.basePath.length - 1) + (request.url.search || '')); //required to preserve query parameters
         }
 
         //Set version for usage in handler
